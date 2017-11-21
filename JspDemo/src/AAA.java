806d8b8f91d43d34b7c3513abcd42c37d23ee7e5
yedashao18 <yedashao18@126.com>
2017-11-21 10:57:54 +0800

    my first git

---
+++ JspDemo/src/AAA.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class AAA {
+	public static void main(String[] args) {
+		int radomInt = new Random().nextInt(999999);
+		System.out.println(radomInt);
+				
+		
+		
+	}
+}
